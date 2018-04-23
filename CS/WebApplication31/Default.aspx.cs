c81c682cafa762693f49e14def1cccf15a0c5a88
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebApplication31/Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using DevExpress.DashboardCommon;
+using DevExpress.DashboardWeb;
+using DevExpress.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication31 {
+    public partial class Default : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            BindListBox();
+        }
+
+        protected void ASPxCallbackPanel1_Callback(object sender, DevExpress.Web.CallbackEventArgsBase e) {
+            ASPxCallbackPanel callbackPanel = (ASPxCallbackPanel)sender;
+            Dashboard dashboard = new Dashboard();
+
+            IEditableDashboardStorage storage = DashboardConfigurator.Default.DashboardStorage as IEditableDashboardStorage;
+            if (storage != null) {
+                string newID = storage.AddDashboard(dashboard.SaveToXDocument(), Guid.NewGuid().ToString());
+                BindListBox();
+                callbackPanel.JSProperties["cpNew"] = newID;
+            }
+        }
+
+        void BindListBox() {
+            ASPxListBox1.DataSource = DashboardConfigurator.Default.DashboardStorage.GetAvailableDashboardsInfo();
+            ASPxListBox1.DataBind();
+        }
+    }
+}
---
+++ VB/WebApplication31/Default.aspx.vb
@@ -0,0 +1,36 @@
+﻿Imports DevExpress.DashboardCommon
+Imports DevExpress.DashboardWeb
+Imports DevExpress.Web
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+
+Namespace WebApplication31
+    Partial Public Class [Default]
+        Inherits System.Web.UI.Page
+
+        Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+            BindListBox()
+        End Sub
+
+        Protected Sub ASPxCallbackPanel1_Callback(ByVal sender As Object, ByVal e As DevExpress.Web.CallbackEventArgsBase)
+            Dim callbackPanel As ASPxCallbackPanel = DirectCast(sender, ASPxCallbackPanel)
+            Dim dashboard As New Dashboard()
+
+            Dim storage As IEditableDashboardStorage = TryCast(DashboardConfigurator.Default.DashboardStorage, IEditableDashboardStorage)
+            If storage IsNot Nothing Then
+                Dim newID As String = storage.AddDashboard(dashboard.SaveToXDocument(), Guid.NewGuid().ToString())
+                BindListBox()
+                callbackPanel.JSProperties("cpNew") = newID
+            End If
+        End Sub
+
+        Private Sub BindListBox()
+            ASPxListBox1.DataSource = DashboardConfigurator.Default.DashboardStorage.GetAvailableDashboardsInfo()
+            ASPxListBox1.DataBind()
+        End Sub
+    End Class
+End Namespace
